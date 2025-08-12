8e024302386dbed55a2d0711ec7289be26837fb0
dzellefaith.tan@gcash.com <dzellefaith.tan@gcash.com>
2025-08-12 08:54:21 +0800

    Exercise 6: Method Overloading

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -5,7 +5,12 @@ public class Converter {
         return (celsius * 9.0 / 5.0) + 32;
     }
 
+    static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0/9.0;
+    }
+
     public static void main(String[] args) {
         System.out.printf("%.1f degrees C is %.1f degrees F.%n", 25.0, celsiusToFahrenheit(25.0));
+        System.out.printf("%.1f degrees F is %.1f degrees C.%n", 77.0, fahrenheitToCelsius(77.0));
     }
 }
