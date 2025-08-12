b47328d0972a0b79731ee947fe153db40a3c9b2a
dzellefaith.tan@gcash.com <dzellefaith.tan@gcash.com>
2025-08-12 08:01:42 +0800

    Exercise 2: Passing Parameters, Primitives vs. Objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import org.example.Student;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void changeStudentName(Student student, String newName) {
+        student.name = newName;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println("Name before change: " + student.name);
+        changeStudentName(student, "Jordan");
+        System.out.println("Name after change: " + student.name);
     }
 }
